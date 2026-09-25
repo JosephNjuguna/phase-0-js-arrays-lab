30803081fc6c8bbdf4ff45a9e4bac2d2b1eaa672
JosephNjuguna <joseph.njuguna5@student.moringaschool.com>
2026-09-25 10:52:05 +0300

    added all the needed functions per instructions

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,30 @@
 // Write your code here
+let products = ["Laptop", "Phone", "Headphones", "Monitor"]
 
 
+function logFirstProduct() {
+  console.log(products[0]);
+  console.log(products);
+}
+
+function addProduct(product) {
+  products.push(product)
+  console.log(products);
+}
+addProduct("keyboard")
+
+function updateProductName(position,product) {
+  products.splice(position,1,product)
+  console.log(products);
+  
+}
+updateProductName(3,"Desktop")
+
+function removeLastProduct(){
+  products.pop()
+  console.log(products);
+  
+}
 
 // Export the necessary parts for testing
 module.exports = {
